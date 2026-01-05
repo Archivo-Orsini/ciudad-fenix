d913678c7b1fe96301b7bfe1c8cd58ad2885fac3
claraharguindey <clarisaharguindey@gmail.com>
2026-01-05 15:52:10 +0100

    Add tab

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const showMenu = () => {
   if (menu.style.display === "none" || menu.style.display === "") {
     menu.style.display = "flex";
     article.style.display = "none";
+    document.querySelector(".article-tab").style.display = "none";
   } else {
     menu.style.display = "none";
   }
@@ -13,11 +14,29 @@ const showMenu = () => {
 const showInfo = () => {
   const article = document.querySelector(".article");
   const menu = document.querySelector(".hidden-menu");
+  const tab = document.querySelector(".article-tab");
 
   if (article.style.display === "none" || article.style.display === "") {
     article.style.display = "block";
     menu.style.display = "none";
+    tab.style.display = "none";
   } else {
     article.style.display = "none";
   }
+};
+
+const minimizeArticle = () => {
+  const article = document.querySelector(".article");
+  const tab = document.querySelector(".article-tab");
+  
+  article.style.display = "none";
+  tab.style.display = "block";
+};
+
+const restoreArticle = () => {
+  const article = document.querySelector(".article");
+  const tab = document.querySelector(".article-tab");
+  
+  article.style.display = "block";
+  tab.style.display = "none";
 };
